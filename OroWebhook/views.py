f9da087870711973bbb4912ea9c0ad7efba8f71a
Marcel Porta Vallés <marcelpv96@gmail.com>
2017-07-20 11:45:41 +0200

    modified views for webhook answer

--- OroWebhook/views.py
+++ OroWebhook/views.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
+from django.http import JsonResponse
 
 from .models import WebhookTransaction
 
@@ -11,8 +12,8 @@ from .models import WebhookTransaction
 @require_POST
 def webhook(request):
 
-    return {
+    return JsonResponse({
         "speech": "a",
         "displayText": "a",
         "source": "API.AI-test-simple-Quiz"
-    }
+    })
