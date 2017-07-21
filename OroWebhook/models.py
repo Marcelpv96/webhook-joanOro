00f1dbb747e501454014bde1521afc25440b99dd
Marcel Porta Vallés <marcelpv96@gmail.com>
2017-07-21 11:02:57 +0200

    models changed

--- OroWebhook/models.py
+++ OroWebhook/models.py
@@ -4,21 +4,31 @@ from django.utils import timezone
 # Create your models here.
 
 
-class WebhookTransaction(models.Model):
-    UNPROCESSED = 1
-    PROCESSED = 2
-    ERROR = 3
-
-    STATUSES = (
-        (UNPROCESSED, 'Unprocessed'),
-        (PROCESSED, 'Processed'),
-        (ERROR, 'Error'),
-    )
-
-    date_generated = models.DateTimeField()
-
+class Question(models.Model):
+    Question = models.TextField(null=False)
 
     def __unicode__(self):
-        return u'{0}'.format(self.date_event_generated)
+        return u'{0}'.format(self.Question)
+
+class Answer(models.Model):
+    Question = models.ForeignKey(Question)
+    Answer = models.TextField(null=False)
+
+    def  __unicode__(self):
+        return u'{0}'.format(self.Answer)
+
+class BadAnswer(models.Model):
+    Answer = models.TextField(null=False)
+
+    def  __unicode__(self):
+        return u'{0}'.format(self.Answer)
+
+class APIAIanswer(models.Model):
+    speech = models.TextField(null=True)
+    displayText = models.TextField(null=True)
+    source = models.CharField(max_length=20, default='Webhook-JoanOro', editable=False)
+
+    def __unicode__(self):
+        return u'{0}'.format(self.speech)
 
 
--- OroWebhook/views.py
+++ OroWebhook/views.py
@@ -4,14 +4,12 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.http import JsonResponse
-
-from .models import WebhookTransaction
+from django.core import serializers
 
 
 @csrf_exempt
 @require_POST
 def webhook(request):
-
     return JsonResponse({
         "speech": "a",
         "displayText": "a",
