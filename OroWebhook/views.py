68f6927e3e2dc3432754e2238587150d407ac027
Marcel Porta Vallés <marcelpv96@gmail.com>
2017-07-24 13:18:38 +0200

    procfile mod // views chang

--- OroWebhook/views.py
+++ OroWebhook/views.py
@@ -6,7 +6,7 @@ from django.views.decorators.http import require_POST
 from django.http import JsonResponse
 from django.core import serializers
 
-
+@csrf_exempt
 @require_POST
 def webhook(request):
     request_data = json.loads(request)[0]
